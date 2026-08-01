1042ac93d30dab68bf6f2b470fee9059caee1d58
Past <past6867@gmail.com>
2026-08-01 15:29:31 -0500

    arranque

---
+++ instalar_dependencias.bat
@@ -0,0 +1,10 @@
+@echo off
+title INSTALADOR DE DEPENDENCIAS - CHIFA POS
+echo ========================================================
+echo   Instalando dependencias requeridas para el Servidor
+echo ========================================================
+echo.
+call npm install
+echo.
+echo [OK] Dependencias instaladas con exito.
+pause
