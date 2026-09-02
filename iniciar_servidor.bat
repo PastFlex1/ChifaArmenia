1efefa9553b71a95281ef14c8cd60efffcca9e03
Past <past6867@gmail.com>
2026-09-01 21:10:41 -0500

    github impresoras

--- iniciar_servidor.bat
+++ iniciar_servidor.bat
@@ -1,5 +1,15 @@
 @echo off
 title SERVIDOR DE IMPRESION MATRIZ - CHIFA MEI HUA
+cd /d "%~dp0"
+
+if not exist "%~dp0node_modules\firebase" (
+    echo ========================================================
+    echo   Instalando dependencias de Firebase por primera vez...
+    echo ========================================================
+    call npm install
+)
+
+echo.
 echo Iniciando el servidor de impresion Matriz...
 if exist "%~dp0node.exe" (
     "%~dp0node.exe" print-server.js
